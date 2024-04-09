9493034f15009027e2cd3b30ece04fd11d59e813
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-04-08 19:34:30 -0700

    Delete bad files

--- src/main/java/survivalblock/enchancement_unbound/access/VeilModifyWorldAccess.java
+++
@@ -1,6 +0,0 @@
-package survivalblock.enchancement_unbound.access;
-
-public interface VeilModifyWorldAccess {
-    void enchancement_unbound$setModifyWorldChanged(boolean value);
-    boolean enchancement_unbound$getModifyWorldChanged();
-}
