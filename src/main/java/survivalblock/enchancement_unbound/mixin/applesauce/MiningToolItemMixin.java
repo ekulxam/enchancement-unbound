c7f2f6513d7f251da74afb63ead2ba841e1c19af
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-05-14 17:43:32 -0700

    Github is being a pain and won't let me upload multiple files

--- src/main/java/survivalblock/enchancement_unbound/mixin/applesauce/MiningToolItemMixin.java
+++ src/main/java/survivalblock/enchancement_unbound/mixin/applesauce/MiningToolItemMixin.java
@@ -1,5 +1,7 @@
 package survivalblock.enchancement_unbound.mixin.applesauce;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -14,6 +16,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -35,18 +38,41 @@ public class MiningToolItemMixin {
         if (!UnboundConfig.unboundEnchantments) {
             return;
         }
-        if (EnchantmentHelper.getLevel(UnboundEnchantments.APPLE_SAUCE, stack) <= 0) {
+        int amplifier = EnchantmentHelper.getLevel(UnboundEnchantments.APPLE_SAUCE, stack);
+        if (amplifier <= 0) {
             return;
         }
         if (!(state.isIn(BlockTags.LEAVES))) {
             return;
         }
+        amplifier--;
         Random random = world.getRandom();
-        if (MathHelper.nextBetween(random, 0, 2) >= 2) {
+        if (MathHelper.nextBetween(random, 0, 9) >= 9 - amplifier) { // 1 in 10
             Block.dropStack(world, pos, Items.APPLE.getDefaultStack());
         }
-        if (MathHelper.nextBetween(random, 0, 9) >= 9) {
-            Block.dropStack(world, pos, Items.GOLDEN_APPLE.getDefaultStack());
+        if (MathHelper.nextBetween(random, 0, 24) >= 24 - amplifier) { // 1 in 25
+            if (MathHelper.nextBetween(random, 0, 4) >= 4 - Math.floor(amplifier / 2d)) { // 1 in 5
+                Block.dropStack(world, pos, Items.ENCHANTED_GOLDEN_APPLE.getDefaultStack());
+            } else {
+                Block.dropStack(world, pos, Items.GOLDEN_APPLE.getDefaultStack()); // 4 in 5
+            }
         }
+    }
+
+    @ModifyExpressionValue(method = "getMiningSpeedMultiplier", at = @At(value = "FIELD", target = "Lnet/minecraft/item/MiningToolItem;miningSpeed:F", opcode = Opcodes.GETFIELD))
+    private float instaMineLeavesMaybe(float original, ItemStack stack, BlockState state){
+        if (!(stack.getItem() instanceof HoeItem)) {
+            return original;
+        }
+        if (!UnboundConfig.unboundEnchantments) {
+            return original;
+        }
+        if (EnchantmentHelper.getLevel(UnboundEnchantments.APPLE_SAUCE, stack) <= 0) {
+            return original;
+        }
+        if (!(state.isIn(BlockTags.LEAVES))) {
+            return original;
+        }
+        return Math.max(15.0f, original);
     }
 }
